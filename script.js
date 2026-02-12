6d4d5c0a1d782ccb57011e1063f68a7e051abbdd
mohdrozaimi205 <35474800+mohdrozaimi205@users.noreply.github.com>
2026-02-12 15:14:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ async function fetchHargaDiesel() {
     if (idxDiesel !== -1) {
       hargaPasaranInput.value = parseFloat(firstRow[idxDiesel]);
     } else {
-      hargaPasaranInput.value = 2.92; // fallback
+      hargaPasaranInput.value = 2.99; // fallback
     }
 
     if (idxSubsidi !== -1) {
@@ -37,7 +37,7 @@ async function fetchHargaDiesel() {
     calculate();
   } catch (err) {
     console.error("Gagal fetch data:", err);
-    hargaPasaranInput.value = 2.92;
+    hargaPasaranInput.value = 2.99;
     hargaSubsidiInput.value = 2.15;
     calculate();
   }
